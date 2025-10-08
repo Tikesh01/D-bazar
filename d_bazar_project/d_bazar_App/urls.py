9de95e1e88c18ae6db11998df8bddfafcf11ce8d
Tikesh sahu <ts2665857@gmail.com>
2025-10-08 08:41:49 +0530

    dfa

--- d_bazar_project/d_bazar_App/urls.py
+++ d_bazar_project/d_bazar_App/urls.py
@@ -1,5 +1,7 @@
 from . import views
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -9,3 +11,6 @@ urlpatterns = [
     path('items/edit',views.edit_items, name='edit_items'),
     path('<name>',views.render_page, name='<name>')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
